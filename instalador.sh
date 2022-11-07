46340017faa52c3db3718b949c23f23301047d8c
emirjorge <117597846+emirjorge@users.noreply.github.com>
2022-11-07 11:00:22 -0500

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -205,18 +205,26 @@ install_mod() {
   echo -e "                      \033[1;41m  menu  \033[0;37m" && msg -bar2
 }
 
+salir() {
+  exit
+}
+
 #MENUS
 /bin/cp /etc/skel/.bashrc ~/
 /bin/cp /etc/skel/.bashrc /etc/bash.bashrc
 
+echo -ne " \e[1;93m [\e[1;32m0\e[1;93m]\033[1;31m > \033[1;97m Salir \e[97m \n"
 echo -ne " \e[1;93m [\e[1;32m1\e[1;93m]\033[1;31m > \033[1;97m INSTALAR 8.6x VPS \e[97m \n"
 
 echo -ne "\033[1;97mDigite solo el numero segun su respuesta:\e[32m "
 read opcao
 case $opcao in
 
+0)
+  salir
+  ;;
+
 1)
   install_mod
   ;;
 esac
-exit
