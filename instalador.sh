70a407071c7f8ca7a27f53e96a42641412fbecb4
emirjorge <117597846+emirjorge@users.noreply.github.com>
2022-11-07 15:00:43 -0500

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-# ROOT
-if [[ "${UID}" -ne 0 ]]; then
-    echo " You need to run this script as root"
-    exit 1
-fi
-
-# To directly modify sshd_config.
-sed -i 's/#\?\(Port\s*\).*$/\1 22/' /etc/ssh/sshd_config
-sed -i 's/#\?\(PermitRootLogin\s*\).*$/\1 yes/' /etc/ssh/sshd_config
-sed -i 's/#\?\(PubkeyAuthentication\s*\).*$/\1 yes/' /etc/ssh/sshd_config
-sed -i 's/#\?\(PermitEmptyPasswords\s*\).*$/\1 no/' /etc/ssh/sshd_config
-sed -i 's/#\?\(PasswordAuthentication\s*\).*$/\1 yes/' /etc/ssh/sshd_config
-
-# Check the exit status of the last command
-if [[ "${?}" -ne 0 ]]; then
-   echo "The sshd_config file was not modified successfully"
-   exit 1
-fi
 
 clear && clear
 
