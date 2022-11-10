f922564471c90cf32cb039f3f4e8133e11bd3dbf
emirjorge <117597846+emirjorge@users.noreply.github.com>
2022-11-10 12:11:27 -0500

    Update instalador.sh

--- instalador.sh
+++ instalador.sh
@@ -15,9 +15,9 @@ echo "$v1" >/etc/versin_script
 v22=$(cat /etc/versin_script)
 vesaoSCT="\033[1;31m [ \033[1;32m($v22)\033[1;97m\033[1;31m ]"
 
-SCPdir="/etc/VPS-MX"
-[[ ! -d ${SCPdir}/tmp ]] && mkdir ${SCPdir}/tmp
 mkdir -p /etc/VPS-MX/protocolos
+SCPdir="/etc/VPS-MX"
+[[ ! -e ${SCPdir}/tmp ]] && mkdir ${SCPdir}/tmp
 
 # LA CONDICIONAL [  ] hace q si existe el archivo, por consecuencia && no hace la accion 
 [[ ! -e /bin/desbloqueo.sh ]] && wget -O /bin/desbloqueo.sh https://raw.githubusercontent.com/emirjorge/premium/master/librerias/desbloqueo.sh &> /dev/null
